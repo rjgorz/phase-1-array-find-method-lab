41d344b15e7498456fa73382d4a382235c5417ec
Robert Gorz <rjgorz@gmail.com>
2023-01-10 16:19:09 -0600

    working function utilizing the find() method to pass test cases

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    const result = record.find(record => record.result === "W");
+    if(result === undefined)
+        return undefined;
+    else
+        return result.year;
+}
